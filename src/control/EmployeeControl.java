c6e52d5fd4f0f31f3a5bb20fca208466ce6d4e31
CTRLMarcws <marfilipi@hotmail.com>
2021-10-26 00:43:51 -0300

    SignUp and employee control improved

--- src/control/EmployeeControl.java
+++ src/control/EmployeeControl.java
@@ -3,15 +3,26 @@ package control;
 import dao.EmployeeDAO;
 import dao.EmployeeDAOImpl;
 import entity.Employee;
+
+import java.util.Date;
+
 import org.bson.types.ObjectId;
 
 public class EmployeeControl {
 
   private EmployeeDAO service = new EmployeeDAOImpl();
 
-  public void create(Employee employee) {}
+  public void create(Employee employee) {
+    service.insert(employee);
+  }
 
-  public void read() {}
+  public void readAll() {}
+
+  public void readByID(ObjectId id) {}
+
+  public void readByDate(String field, Date dateGte, Date dateLte) {}
+
+  public void readByField(String field, String data) {}
 
   public void update(ObjectId id, Employee employee) {}
 
--- src/control/SignUpControl.java
+++ src/control/SignUpControl.java
@@ -2,6 +2,7 @@ package control;
 
 import dao.EmployeeDAO;
 import dao.EmployeeDAOImpl;
+import entity.Employee;
 
 public class SignUpControl {
 
@@ -14,30 +15,48 @@ public class SignUpControl {
     String password
   ) {
     if (service.findToCreateUser(username, email)) {
-      // doSomething
+      Employee user = new Employee(email, username, fullname, password);
+      service.insert(user);
     }
   }
 
-  public void passwordVerification(String password) {
+  public boolean passwordVerification(String password) {
     /* 
 		rules:
 		- at least 8 characters
 		- must contain at least:
-			1 letter;
-			1 digit;
-			1 symbol/special char.
+    1 uppercase letter;
+    1 lowercase letter;
+    1 digit;
+    1 symbol/special char.
 		- up to 15 characters
 		*/
     if (password.length() > 7 && password.length() < 16) {
       String specialPattern = "!#$&*+_-@";
-      boolean valid[] = new boolean[3];
+
+      boolean hasLowercase = false;
+      boolean hasUppercase = false;
+      boolean hasDigit = false;
+      boolean hasSpecialCharacter = false;
+      // boolean hasInvalidCharacter = false;
+
       for (char c : password.toCharArray()) {
-        if (Character.isDigit(c)) valid[0] = true; else if (
-          Character.isLetter(c)
-        ) valid[1] = true;
-        // else if valid special character valid [2] = true;
+        if (
+          hasLowercase && hasUppercase && hasDigit && hasSpecialCharacter
+        ) break;
+
+        if (Character.isLowerCase(c)) hasLowercase = true;
+        else if (Character.isUpperCase(c)) hasUppercase = true;
+        else if (Character.isDigit(c)) hasDigit = true;
+        else if (specialPattern.contains(Character.toString(c))) hasSpecialCharacter = true;
+        else return false; //has invalid characters
+      }
+
+      if (hasLowercase && hasUppercase && hasDigit && hasSpecialCharacter) {
+        return true;
       }
     }
+    return false; //the password does not attend one of the rules
   }
 
   public boolean passwordConfirmation(
