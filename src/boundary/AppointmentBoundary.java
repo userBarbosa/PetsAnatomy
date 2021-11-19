5db53eb2ab199cb3335f054da19c5e3e66c4da55
anabalves <ana.alves41@fatec.sp.gov.br>
2021-11-18 21:10:46 -0300

    Create popup find by date

--- src/boundary/AppointmentBoundary.java
+++ src/boundary/AppointmentBoundary.java
@@ -13,6 +13,7 @@ import javafx.beans.binding.Bindings;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -26,6 +27,8 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.converter.NumberStringConverter;
 import org.bson.types.ObjectId;
 
@@ -44,7 +47,13 @@ public class AppointmentBoundary implements StrategyBoundary {
   private ComboBox<String> cbEmployee = new ComboBox<>();
   private ComboBox<String> cbState = new ComboBox<>();
   private ComboBox<String> cbFinancialState = new ComboBox<>();
-
+  
+  private Label lblDateGte = new Label("Data Inicial");
+  private Label lblDateLte = new Label("Data Final");
+  private DatePicker dpDateGte = new DatePicker();
+  private DatePicker dpDateLte = new DatePicker();
+  private Button btnDismiss = new Button("Buscar");
+  
   private Label lblId = new Label("Id");
   private Label lblDate = new Label("Data");
   private Label lblTime = new Label("Horário");
@@ -59,9 +68,14 @@ public class AppointmentBoundary implements StrategyBoundary {
   private Button btnClear = new Button("Limpar");
   private Button btnUpdate = new Button("Atualizar");
   private Button btnFind = new Button("Pesquisar");
+  private Button btnFindByDate = new Button("Pesquisar por Data");
   private Button btnCreate = new Button("Adicionar");
   private Button btnDelete = new Button("Remover");
 
+  Font fontBtns = Font.loadFont("file:resources/fonts/Poppins-Regular.ttf", 12);
+  Font fontLbls = Font.loadFont("file:resources/fonts/Poppins-Regular.ttf", 12);
+  Font fontTf = Font.loadFont("file:resources/fonts/Poppins-Regular.ttf", 12);
+  
   private TableView<Appointment> table = new TableView<Appointment>();
   private static AppointmentControl control = new AppointmentControl();
   private WorkShift ws = new WorkShift();
@@ -181,16 +195,6 @@ public class AppointmentBoundary implements StrategyBoundary {
   @Override
   public Pane generateBoundaryStrategy() {
     AnchorPane formPane = new AnchorPane();
-
-    Font fontBtns = Font.loadFont(
-      "file:resources/fonts/Poppins-Regular.ttf",
-      12
-    );
-    Font fontLbls = Font.loadFont(
-      "file:resources/fonts/Poppins-Regular.ttf",
-      12
-    );
-    Font fontTf = Font.loadFont("file:resources/fonts/Poppins-Regular.ttf", 12);
 
     binding();
 
@@ -343,6 +347,7 @@ public class AppointmentBoundary implements StrategyBoundary {
         tfObs,
         btnCreate,
         btnFind,
+        btnFindByDate,
         btnUpdate,
         btnDelete,
         btnClear,
@@ -355,21 +360,7 @@ public class AppointmentBoundary implements StrategyBoundary {
     btnCreate.setLayoutX(24.0);
     btnCreate.setLayoutY(246.0);
     btnCreate.setFont(fontBtns);
-
-    btnFind.setOnAction((e) -> {
-        control.findByField();
-    });
-    btnFind.setLayoutX(290.0);
-    btnFind.setLayoutY(246.0);
-    btnFind.setFont(fontBtns);
-
-    btnUpdate.setOnAction((e) -> {
-        control.updateById();
-    });
-    btnUpdate.setLayoutX(205.0);
-    btnUpdate.setLayoutY(246.0);
-    btnUpdate.setFont(fontBtns);
-
+    
     btnDelete.setOnAction(
     		(e) -> {
     	  if (tfId.getText() == "" || tfId.getText()== null) {
@@ -390,18 +381,86 @@ public class AppointmentBoundary implements StrategyBoundary {
     btnDelete.setLayoutX(114.0);
     btnDelete.setLayoutY(246.0);
     btnDelete.setFont(fontBtns);
+    
+    btnUpdate.setOnAction((e) -> {
+        control.updateById();
+    });
+    btnUpdate.setLayoutX(205.0);
+    btnUpdate.setLayoutY(246.0);
+    btnUpdate.setFont(fontBtns);
+
+    btnFind.setOnAction((e) -> {
+        control.findByField();
+    });
+    btnFind.setLayoutX(290.0);
+    btnFind.setLayoutY(246.0);
+    btnFind.setFont(fontBtns);
+    
+    btnFindByDate.setOnAction((e) -> {
+    	this.findByDatePopup();
+//        control.findByDate();
+    });
+    btnFindByDate.setLayoutX(380.0);
+    btnFindByDate.setLayoutY(246.0);
+    btnFindByDate.setFont(fontBtns);
 
     btnClear.setOnAction((e) -> {
         control.clearFields();
     });
-    btnClear.setLayoutX(459.0);
+    btnClear.setLayoutX(540.0);
     btnClear.setLayoutY(246.0);
     btnClear.setFont(fontBtns);
 
     return formPane;
   }
 
-  private void binding() {
+  private void findByDatePopup() {
+	  Stage popup = new Stage();
+	  popup.initModality(Modality.WINDOW_MODAL);
+
+	  AnchorPane pane = new AnchorPane();
+	  Scene scene = new Scene(pane, 420, 100);
+	  popup.setScene(scene);
+
+	  lblDateGte.setLayoutX(25.0);
+	  lblDateGte.setLayoutY(20.0);
+	  lblDateGte.setPrefHeight(17.0);
+	  lblDateGte.setPrefWidth(100.0);
+	  lblDateGte.setFont(fontLbls);
+
+	  dpDateGte.setLayoutX(110.0);
+	  dpDateGte.setLayoutY(17.0);
+	  dpDateGte.setPrefHeight(25.0);
+	  dpDateGte.setPrefWidth(100.0);
+
+	  lblDateLte.setLayoutX(220.0);
+	  lblDateLte.setLayoutY(20.0);
+	  lblDateLte.setPrefHeight(17.0);
+	  lblDateLte.setPrefWidth(100.0);
+	  lblDateLte.setFont(fontLbls);
+
+	  dpDateLte.setLayoutX(290.0);
+	  dpDateLte.setLayoutY(17.0);
+	  dpDateLte.setPrefHeight(25.0);
+	  dpDateLte.setPrefWidth(100.0);	  
+
+	  btnDismiss.setOnAction((e) -> {
+		  popup.close();
+	  });
+	  btnDismiss.setLayoutX(25.0);
+	  btnDismiss.setLayoutY(60.0);
+	  btnDismiss.setFont(fontBtns);
+
+	  pane
+	  .getChildren()
+	  .addAll(btnDismiss, lblDateGte, dpDateGte, lblDateLte, dpDateLte);
+
+	  popup.setTitle("Pesquisar consulta por data");	
+	  popup.setResizable(false);
+	  popup.showAndWait();
+  }
+
+private void binding() {
     Bindings.bindBidirectional(tfId.textProperty(), control.idProperty());
     Bindings.bindBidirectional(dpDate.valueProperty(), control.dateProperty());
     Bindings.bindBidirectional(cbTime.valueProperty(), control.timeProperty());
